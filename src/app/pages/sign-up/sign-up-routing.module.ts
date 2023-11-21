e5f1afe3699a1f9312db688ade840fc91ae3c807
Ananthu Ajayan <137896002+ananthuajayan@users.noreply.github.com>
2023-11-21 11:28:49 +0530

    errors

--- src/app/pages/sign-up/sign-up-routing.module.ts
+++ src/app/pages/sign-up/sign-up-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up.component';
 
 const routes: Routes = [{
-  path:'signup' ,
+  path:'' ,
    component: SignUpComponent
 }];
 
