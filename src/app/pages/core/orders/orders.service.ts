2b7282c57470d4ed44b73c705195478f711fc946
abhi <mkabhijith149@gmail.com>
2023-11-29 13:09:47 +0530

    create b order component

---
+++ src/app/pages/core/orders/orders.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrdersService {
+  constructor() {}
+  orderList: any[] = [
+    {
+      id: 1,
+      imageUrl: '/assets/images/food/Kuzhalappam.jpg',
+      productName: 'Kuzhalappam',
+      price: 75,
+      description:
+        ' Ingredients: Rice flour, granted coconut, blak seasame seeds, cumin seeds, crushed black pepper, asafoetida, salt, water, oil Zero Preservatives/Artificial Colours/ Flavours/ Sugars',
+      quantity: '175g',
+    },
+    {
+      id: 1,
+      imageUrl: '/assets/images/food/Kuzhalappam.jpg',
+      productName: 'Kuzhalappam',
+      price: 75,
+      description:
+        ' Ingredients: Rice flour, granted coconut, blak seasame seeds, cumin seeds, crushed black pepper, asafoetida, salt, water, oil Zero Preservatives/Artificial Colours/ Flavours/ Sugars',
+      quantity: '175g',
+    },
+  ];
+  returnOrderList(){
+    return this.orderList;
+  }
+}
