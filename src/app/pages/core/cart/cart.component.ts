029e3936a01c25a893d77f17717f54a7a2eff2eb
abhi <mkabhijith149@gmail.com>
2023-12-18 14:22:42 +0530

    update cart

--- src/app/pages/core/cart/cart.component.ts
+++ src/app/pages/core/cart/cart.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CartService } from './cart.service';
+import { StorageService } from 'src/app/shared/services/storage/storage.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  constructor(
+    private cartService: CartService,
+    private storageService: StorageService
+  ) {}
 
+  cartListSubscription!: Subscription;
+  cartList: any[] = [];
+  ngOnInit(): void {
+    // this.cartService.getCart();
+    console.log('cart oninit');
+
+    // this.cartService.getCart();
+    this.cartListSubscription = this.cartService.cartList$.subscribe({
+      next: (res) => {
+        console.log(res);
+
+        this.cartList = res;
+        console.log('cartlist', this.cartList);
+      },
+    });
+    console.log('ng', this.cartList);
+
+    console.log('after', this.cartList.length);
+  }
+
+  onItemRemove(id: number) {
+    this.cartService.onCartRemoveItem(id);
+  }
 }
